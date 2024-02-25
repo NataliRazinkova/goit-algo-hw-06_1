87ae6f19eb0ed3e7ffc6b1268124b726817b626a
NataliRazinkova <155454165+NataliRazinkova@users.noreply.github.com>
2024-02-25 14:31:22 +0200

    Create Homework_6.1.py

---
+++ Homework_6.1.py
@@ -0,0 +1,55 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if len(value) != 10 or not value.isdigit():
+            raise ValueError("Phone number must be 10 digits")
+        super().__init__(value)
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone_number):
+        self.phones.append(Phone(phone_number))
+
+    def remove_phone(self, phone_number):
+        phones_to_remove = [phone for phone in self.phones if phone.value == phone_number]
+        for phone in phones_to_remove:
+            self.phones.remove(phone)
+
+    def edit_phone(self, old_phone_number, new_phone_number):
+        for phone in self.phones:
+            if phone.value == old_phone_number:
+                phone.value = new_phone_number
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def search_record(self, name):
+        if name in self.data:
+            return self.data[name]
+        else:
+            return None
+
+    def remove_record(self, name):
+        if name in self.data:
+            del self.data[name]
+
+    def __str__(self):
+        return '\n'.join(str(record) for record in self.data.values())
